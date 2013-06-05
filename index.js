e96de8794da804ebc88bb3aef63f8d9534c3aa34
Mark McKenna <mark.denis.mckenna@gmail.com>
2013-06-04 23:37:07 -0300

    Still setting up environment

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // Allows you to present a datamodel in an active DOM-like structure.
 // Each element 
 
-// Export other elements
-exports.Model = require('model');
-exports.Element = require('element');
-exports.Attribute = require('attribute');
+var dom = require('jsdom/level1/core').dom.level1.core;
+var fs = require('fs');
+
+// First things first... serialize/deserialize the data model.
+exports = module.exports = function(file, options) {
+	var hnd = fs.open(file, 'r');
+	var doc = dom(hnd);
+}
